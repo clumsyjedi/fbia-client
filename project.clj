1cb1b016283561ee3c677362746afa7a0f943e1f
Frazer Irving <frazer.irving@mailonline.com>
2018-07-02 11:26:13 +1000

    Version 0.4.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fbia-client "0.4.6-SNAPSHOT"
+(defproject fbia-client "0.4.6"
   :description "Facebook instant articles client in clojure"
   :url "https://github.com/clumsyjedi/fbia-client"
   :license {:name "Eclipse Public License"
