f54e087de9f8d45e322f6f9f93eb697ee9e4884c
Frazer Irving <frazer.irving@mailonline.com>
2020-11-12 11:15:05 +1100

    chore: bumps core.async version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.1.18"]
-                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.async "1.3.610"]
                  [org.clojure/data.json "0.2.6"]
                  [org.apache.httpcomponents/httpclient "4.5.2"]]
   :plugins [[lein-codox "0.10.2"]])
