9d4c249bd8f500c5b8a5a71c7a58d78ef3fb736b
Frazer Irving <frazer.irving@mailonline.com>
2020-11-23 12:05:29 +1100

    chore: bumps version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fbia-client "0.5.0"
+(defproject fbia-client "0.5.1"
   :description "Facebook instant articles client in clojure"
   :url "https://github.com/clumsyjedi/fbia-client"
   :license {:name "Eclipse Public License"
